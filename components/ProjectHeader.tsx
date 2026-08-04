280a72a7c0a5eb54072f4399f5f9e26517aa4731
thi-nguyen167 <athi.nguyen167@gmail.com>
2026-08-04 16:39:27 -0600

    feat: update the ProjectHeader UI

--- components/ProjectHeader.tsx
+++ components/ProjectHeader.tsx
@@ -10,14 +10,16 @@ export default function ProjectHeader({
   description,
 }: ProjectHeaderProps) {
   return (
-    <div className="my-6 md:my-12 w-full max-w-4xl text-center md:text-left">
+    <div className="my-6 md:my-12 w-full max-w-4xl text-center flex flex-col items-center">
       <span className="font-label-xs text-[11px] uppercase tracking-widest text-secondary mb-4 block font-semibold">
         Challenge {challengeNumber}
       </span>
       <h1 className="font-semibold font-headline-xl text-[48px] md:text-[56px] text-primary mb-4 tracking-tight">
         {title}
       </h1>
-      <p className="font-body-lg text-body-lg text-secondary">{description}</p>
+      <p className="font-body-lg text-body-lg text-secondary max-w-lg">
+        {description}
+      </p>
     </div>
   );
 }
